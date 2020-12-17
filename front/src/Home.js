3d9d13b94fb825f842a718a5e9cabda848eac435
Saveriu Battesti <saveriubattesti@gmail.com>
2020-12-17 10:40:58 +0100

    home vide

---
+++ front/src/Home.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React, { Component } from 'react';
+
+//const API = 'https://us-central1-callert-b38f5.cloudfunctions.net/webApi/api/v1/users/';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }; 
+  }
+
+  render() {
+    return (
+        <h1>Home</h1>
+    );
+  }
+}
+
+export default Home;
